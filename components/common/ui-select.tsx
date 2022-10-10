0bf7609ec7fb2ab13602fc650c672f19f456ff22
YAO YINGYING <yao3060@gmail.com>
2022-10-11 01:14:43 +0800

    add  scrollTo

--- components/common/ui-select.tsx
+++ components/common/ui-select.tsx
@@ -61,32 +61,35 @@ export default function UISelect(props: Props) {
           />
         </span>
       </div>
-      <ul
-        className={`absolute border rounded-lg bg-white text-gray-500 shadow-lg min-w-[200px] max-h-60 overflow-y-auto top-[50px] z-50 ${
-          show ? "block" : "hidden"
+      <div
+        className={`absolute border rounded-lg bg-white text-gray-500 shadow-lg min-w-[200px] overflow-y-auto top-[50px] z-50
+        transition-all duration-300 ease-in-out block  ${
+          show ? "visible h-60" : "invisible h-0"
         }`}
       >
-        {props.options.map((option) => (
-          <li
-            key={option.value}
-            onClick={(e) => {
-              e.stopPropagation();
-              handleSelect(option);
-              setShow(false);
-            }}
-            data-value={option.value}
-            className={classNames(
-              "flex items-center px-2.5 py-2 cursor-pointer hover:bg-slate-100 border-b",
-              { "bg-slate-100": isSelected(option) }
-            )}
-          >
-            <span className="mr-2.5">
-              {isSelected(option) ? <HiCheck /> : <HiChevronRight />}
-            </span>
-            <span>{option.label}</span>
-          </li>
-        ))}
-      </ul>
+        <ul>
+          {props.options.map((option) => (
+            <li
+              key={option.value}
+              onClick={(e) => {
+                e.stopPropagation();
+                handleSelect(option);
+                setShow(false);
+              }}
+              data-value={option.value}
+              className={classNames(
+                "flex items-center px-2.5 py-2 cursor-pointer hover:bg-slate-100 border-b",
+                { "bg-slate-100": isSelected(option) }
+              )}
+            >
+              <span className="mr-2.5">
+                {isSelected(option) ? <HiCheck /> : <HiChevronRight />}
+              </span>
+              <span>{option.label}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
