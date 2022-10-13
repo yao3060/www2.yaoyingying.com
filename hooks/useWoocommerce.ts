459f221a39a899f63c554a91bc5f2cc1b26baec4
YAO YINGYING <yao3060@gmail.com>
2022-10-14 03:15:51 +0800

    add @types/woocommerce__woocommerce-rest-api

--- hooks/useWoocommerce.ts
+++ hooks/useWoocommerce.ts
@@ -4,8 +4,8 @@ import { WP_ROOT_URL } from "utils/constants";
 const useWoocommerce = () => {
   const api = new WooCommerceRestApi({
     url: WP_ROOT_URL,
-    consumerKey: process.env.WOO_KEY,
-    consumerSecret: process.env.WOO_SECRET,
+    consumerKey: process.env.WOO_KEY as string,
+    consumerSecret: process.env.WOO_SECRET as string,
     version: "wc/v3",
   });
 
