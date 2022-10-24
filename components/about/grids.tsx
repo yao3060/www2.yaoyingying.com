a35bf1f674088c5823f3fee60c7992da952113c8
YAO YINGYING <yao3060@gmail.com>
2022-10-25 04:46:17 +0800

    add framer-motion 2

--- components/about/grids.tsx
+++ components/about/grids.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { motion } from "framer-motion";
 import { FaRegThumbsUp, FaRegLightbulb, FaRegEye } from "react-icons/fa";
 
 export default function Grids() {
@@ -6,21 +7,33 @@ export default function Grids() {
     <div className="bg-cyan-500 py-36">
       <div className="container m-auto">
         <div className="grid grid-cols-3 gap-10 text-center  font-light">
-          <div className="shadow py-20 bg-white">
+          <motion.div
+            whileHover={{ scale: 1.05, borderRadius: "10px" }}
+            transition={{ duration: 0.5 }}
+            className="shadow py-20 bg-white"
+          >
             <FaRegThumbsUp className="text-emerald-500 text-[72px] m-auto" />
             <h3 className="text-2xl py-5">Responsibility</h3>
             <p className="text-lg text-gray-400">We are serious people</p>
-          </div>
-          <div className="shadow py-20 bg-white">
+          </motion.div>
+          <motion.div
+            whileHover={{ scale: 1.05, borderRadius: "10px" }}
+            transition={{ duration: 0.5 }}
+            className="shadow py-20 bg-white"
+          >
             <FaRegLightbulb className="text-emerald-500 text-[72px] m-auto" />
             <h3 className="text-2xl py-5">Responsibility</h3>
             <p className="text-lg text-gray-400">We are passionnate</p>
-          </div>
-          <div className="shadow py-20 bg-white">
+          </motion.div>
+          <motion.div
+            whileHover={{ scale: 1.05, borderRadius: "10px" }}
+            transition={{ duration: 0.5 }}
+            className="shadow py-20 bg-white"
+          >
             <FaRegEye className="text-emerald-500 text-[72px] m-auto" />
             <h3 className="text-2xl py-5">Responsibility</h3>
             <p className="text-lg text-gray-400">We are determined</p>
-          </div>
+          </motion.div>
         </div>
       </div>
     </div>
