331c6164ddfd524306ce3b9534dc1c9fdc078362
YAO YINGYING <yao3060@gmail.com>
2022-11-08 07:24:03 +0800

    fix IronSession type

--- next-env.d.ts
+++ next-env.d.ts
@@ -3,3 +3,12 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
+
+import { User } from "hooks/useUser";
+import * as IronSession from "iron-session";
+
+declare module "iron-session" {
+  interface IronSessionData {
+    user?: User;
+  }
+}
