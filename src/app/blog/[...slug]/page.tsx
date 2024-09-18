31dd09466fa9592914944a25c647e4830ed4d498
YAO YINGYING <yao.yingying@it-consultis.net>
2024-09-18 15:19:39 +0800

    add metadata

--- src/app/blog/[...slug]/page.tsx
+++ src/app/blog/[...slug]/page.tsx
@@ -1,12 +1,29 @@
 import { WPPost } from "@/wordpress/wordpress";
 import { wpClient } from "@/wordpress/WPClient";
 import last from "lodash.last";
+import type { Metadata } from "next";
 
 export const dynamic = "force-static";
 
 type PageProps = {
   params: { slug: string[] };
 };
+
+export async function generateMetadata({
+  params,
+}: PageProps): Promise<Metadata> {
+  const slug = last(params.slug);
+
+  const response = await wpClient.fetch(`/wp-json/wp/v2/posts?slug=${slug}`);
+  const [post] = (await response.json()) as WPPost[];
+
+  if (!post) return {};
+
+  return {
+    title: post.title.rendered,
+    description: post.excerpt.rendered.trim().replace(/<[^>]*>?/gm, ""),
+  };
+}
 
 export default async function Page({ params }: PageProps) {
   const slug = last(params.slug);
